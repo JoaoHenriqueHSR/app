3611a15f5073dc3d6d639a66dc430569e8af1c1b
JoaoHenriqueHSR <jh6649713@gmail.com>
2024-09-14 23:00:44 -0300

    listar metas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const {select, input}=require("@inquirer/prompts")
+const {select, input, checkbox}=require("@inquirer/prompts")
 
 let meta={
     value:"correr",
-    checked:true
+    checked:false
 }
 
 let metas=[meta];
@@ -14,6 +14,28 @@ const cadastrarMeta=async()=>{
         return 
     }
     metas.push({value:meta, checked:false})
+}
+
+const listarMetas=async()=>{
+    const respostas= await checkbox({
+        message:"use as setas para andar de meta, o espaço para marcar e desmarcar, e o enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions:false
+    })
+    if(respostas.length==0){
+        console.log("nenhuma meta selecionada")
+        return
+    }
+    metas.forEach((m)=>{
+        m.checked=false
+    })
+    respostas.forEach((resposta)=>{
+        const meta=metas.find((m)=>{
+            return m.value==resposta
+        })
+        meta.checked=true
+    })
+    console.log("meta(s) marcada(s) como concluida(s)")
 }
 
 const start=async()=>{// async é dado pois não se sabe quanto tempo vai demorar ate obter uma respota na linha 6
@@ -42,7 +64,7 @@ const start=async()=>{// async é dado pois não se sabe quanto tempo vai demora
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar");
+                await listarMetas()
                 break
             case "sair":
                 console.log(" saindo ↻")
